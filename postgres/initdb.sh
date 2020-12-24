2cd33a61ffb101f318413e13e4a3b174d63ba694
serhat <serhat@poilabs.com>
2020-12-24 22:23:12 +0300

    version 0.1

--- postgres/initdb.sh
+++ postgres/initdb.sh
@@ -8,10 +8,10 @@
 #     CREATE DATABASE onprem;
 #     GRANT ALL PRIVILEGES ON DATABASE onprem TO admin;
 # EOSQL
-set -e
+# set -e
 
-psql -v ON_ERROR_STOP=1 --username "postgres" <<-EOSQL
-    CREATE USER admin;
-    CREATE DATABASE onprem;
-    GRANT ALL PRIVILEGES ON DATABASE onprem TO admin;
-EOSQL
+# psql -v ON_ERROR_STOP=1 --username "postgres" <<-EOSQL
+#     CREATE USER admin;
+#     CREATE DATABASE onprem;
+#     GRANT ALL PRIVILEGES ON DATABASE onprem TO admin;
+# EOSQL
